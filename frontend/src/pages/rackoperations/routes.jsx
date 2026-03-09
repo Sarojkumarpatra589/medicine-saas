ddfa131acd18abebf9b4469db1bbdc149dad7555
Sarmistha <sarmistha@gmail.com>
2026-03-09 12:41:21 +0530

    add links

--- frontend/src/pages/rackoperations/routes.jsx
+++ frontend/src/pages/rackoperations/routes.jsx
@@ -1,3 +1,5 @@
+import DashboardLayout from "../../components/layout/DashboardLayout";
+
 import { ROUTES } from "../../constants/routes";
 
 import RackDashboard from "./RackDashboard";
@@ -11,17 +13,51 @@ import RackExpiryAlerts from "./RackExpiryAlert";
 import RackBarcodeGenerator from "./RackBarcodeGenerator";
 import RackMovementHistory from "./RackMovementHistory";
 
-const RackOperationsRoutes = [
-  { path: ROUTES.RACK_DASHBOARD, element: <RackDashboard /> },
-  { path: ROUTES.RACK_LIST, element: <RackList /> },
-  { path: ROUTES.RACK_FORM, element: <RackForm /> },
-  { path: ROUTES.SHELF_MANAGEMENT, element: <ShelfManagement /> },
-  { path: ROUTES.RACK_LAYOUT, element: <RackLayout /> },
-  { path: ROUTES.MEDICINE_RACK_ASSIGNMENT, element: <MedicineRackAssignment /> },
-  { path: ROUTES.RACK_INVENTORY, element: <RackInventory /> },
-  { path: ROUTES.RACK_EXPIRY_ALERTS, element: <RackExpiryAlerts /> },
-  { path: ROUTES.RACK_BARCODE_GENERATOR, element: <RackBarcodeGenerator /> },
-  { path: ROUTES.RACK_MOVEMENT_HISTORY, element: <RackMovementHistory /> },
+export default [
+  {
+    element: <DashboardLayout />,
+    children: [
+      // 🗄️ RACK OPERATIONS PAGES
+      {
+        path: ROUTES.RACK_DASHBOARD,
+        element: <RackDashboard />,
+      },
+      {
+        path: ROUTES.RACK_LIST,
+        element: <RackList />,
+      },
+      {
+        path: ROUTES.RACK_FORM,
+        element: <RackForm />,
+      },
+      {
+        path: ROUTES.SHELF_MANAGEMENT,
+        element: <ShelfManagement />,
+      },
+      {
+        path: ROUTES.RACK_LAYOUT,
+        element: <RackLayout />,
+      },
+      {
+        path: ROUTES.MEDICINE_RACK_ASSIGNMENT,
+        element: <MedicineRackAssignment />,
+      },
+      {
+        path: ROUTES.RACK_INVENTORY,
+        element: <RackInventory />,
+      },
+      {
+        path: ROUTES.RACK_EXPIRY_ALERTS,
+        element: <RackExpiryAlerts />,
+      },
+      {
+        path: ROUTES.RACK_BARCODE_GENERATOR,
+        element: <RackBarcodeGenerator />,
+      },
+      {
+        path: ROUTES.RACK_MOVEMENT_HISTORY,
+        element: <RackMovementHistory />,
+      },
+    ],
+  },
 ];
-
-export default RackOperationsRoutes;
