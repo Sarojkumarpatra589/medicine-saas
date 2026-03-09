000fc294a440c958e944de076f2e9d9fb811d166
Sarmistha <sarmistha@gmail.com>
2026-03-09 12:41:51 +0530

    add links

--- frontend/src/pages/racksettings/routes.jsx
+++ frontend/src/pages/racksettings/routes.jsx
@@ -1,16 +1,38 @@
-import { ROUTES } from "../../constants/routes";
+import DashboardLayout from "../../components/layout/DashboardLayout";
+
 import RackSettings from "./RackSettings";
 import RackCategoryMapping from "./RackCategoryMapping";
 import RackBarcodeSettings from "./RackBarcodeSettings";
 import RackAlertSettings from "./RackAlertSettings";
 import RackLayoutSettings from "./RackLayoutSettings";
 
-const RackSettingsRoutes = [
-  { path: ROUTES.RACK_SETTINGS, element: <RackSettings /> },
-  { path: ROUTES.RACK_CATEGORY_MAPPING, element: <RackCategoryMapping /> },
-  { path: ROUTES.RACK_BARCODE_SETTINGS, element: <RackBarcodeSettings /> },
-  { path: ROUTES.RACK_ALERT_SETTINGS, element: <RackAlertSettings /> },
-  { path: ROUTES.RACK_LAYOUT_SETTINGS, element: <RackLayoutSettings /> },
-];
+import { ROUTES } from "../../constants/routes";
 
-export default RackSettingsRoutes;
+export default [
+  {
+    element: <DashboardLayout />,
+    children: [
+      // 🗄️ RACK SETTINGS PAGES
+      {
+        path: ROUTES.RACK_SETTINGS,
+        element: <RackSettings />,
+      },
+      {
+        path: ROUTES.RACK_CATEGORY_MAPPING,
+        element: <RackCategoryMapping />,
+      },
+      {
+        path: ROUTES.RACK_BARCODE_SETTINGS,
+        element: <RackBarcodeSettings />,
+      },
+      {
+        path: ROUTES.RACK_ALERT_SETTINGS,
+        element: <RackAlertSettings />,
+      },
+      {
+        path: ROUTES.RACK_LAYOUT_SETTINGS,
+        element: <RackLayoutSettings />,
+      },
+    ],
+  },
+];
